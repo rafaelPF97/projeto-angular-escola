a50911b6a9d25369926fa4049abd08effd486c0b
Rafael Passarello Fernandes <rafapf97@gmail.com>
2024-12-08 03:26:23 -0300

    update version to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import {
+  provideHttpClient
+} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { SchoolListComponent } from './components/school-list/school-list.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SchoolListComponent } from './components/school-list/school-list.component';
 
-import { SchoolCreateComponent } from './components/school-create/school-create.component';
 import { SchoolEditComponent } from './components/edit/school-aluno-edit/school-edit.component';
+import { SchoolCreateComponent } from './components/school-create/school-create.component';
 
-import { SchoolFormComponent } from './components/school-form/school-form.component';
-import { StudentComponent } from './components/pages/student/student.component';
-import { TeacherComponent } from './components/pages/teacher/teacher.component';
+import { SchoolDiretorEditComponent } from './components/edit/school-diretor-edit/school-diretor-edit.component';
 import { SchoolProfessorEditComponent } from './components/edit/school-professor-edit/school-professor-edit.component';
 import { DirectorComponent } from './components/pages/director/director.component';
-import { SchoolDiretorEditComponent } from './components/edit/school-diretor-edit/school-diretor-edit.component';
+import { StudentComponent } from './components/pages/student/student.component';
+import { TeacherComponent } from './components/pages/teacher/teacher.component';
+import { SchoolFormComponent } from './components/school-form/school-form.component';
 
 @NgModule({
   declarations: [
@@ -40,8 +41,8 @@ import { SchoolDiretorEditComponent } from './components/edit/school-diretor-edi
     DirectorComponent,
     SchoolDiretorEditComponent,
   ],
+  bootstrap: [AppComponent],
   imports: [
-    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -53,9 +54,7 @@ import { SchoolDiretorEditComponent } from './components/edit/school-diretor-edi
     MatDialogModule,
     FormsModule,
     ReactiveFormsModule,
-
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [provideHttpClient(), { provide: Window, useValue: window }],
 })
 export class AppModule {}
